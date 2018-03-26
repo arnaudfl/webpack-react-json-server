602829ff5fa293e23324f5641d7013a0a42283f6
Arnaud F <afenerol@outlook.fr>
2018-03-26 13:36:20 +0200

    generate default app with create react app, add and configure webpack config, add HMR

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,90 @@
 var path = require('path');
 var webpack = require("webpack");
 
-// Webpack Config Documentation: https://webpack.js.org/configuration/
+// webpack plugins
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'public'),
+  output: path.join(__dirname, 'public/compiled')
+};
 
 module.exports = {
-  entry: '',
+  devtool: 'eval-source-map',
+  entry: [
+    'react-hot-loader/patch',
+    PATHS.src
+  ],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    path: PATHS.dist,
+    filename: 'bundle.js',
+    publicPath: '/compiled'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        include:  [PATHS.src],
+        enforce: 'pre',
+        options: {
+          fix: true
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        },
+        include: [PATHS.src],
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader?importLoaders=1&sourceMap',
+          ]
+        })
+      },
+      {
+        test: /\.(ttf|eot|svg|woff|woff2)(\?.*)?$/,
+        use: [
+          'file-loader?name=fonts/[name].[ext]',
+        ],
+        include: [
+          PATHS.src,
+        ]
+      }
+    ]
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 9000,
+    historyApiFallback: true,
+    contentBase: PATHS.dist,
+    hot: true,
+    inline: true,
+    progress: true,
+
+    host: 'localhost',
+    port: 8081,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false
+      '/api/**': {
+        target: 'http://localhost:4000',
+        secure: false,
+        changeOrigin: true
       }
     }
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('bundle.css')
+  ]
 };
